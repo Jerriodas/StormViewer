d1beefd6a3385437f7fb6dfb117de3624c335856
Jerriodas <zeus12gerardo@gmail.com>
2019-12-29 01:15:53 -0600

    create file Manager

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -12,7 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let fileManager = FileManager.default
+        let paths = Bundle.main.resourcePath
+        
+        guard let path = paths else { return }
+        let items = try? fileManager.contentsOfDirectory(atPath: path)
+        
+        if let items = items {
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    //Load picture
+                }
+            }
+        }
     }
 
 
