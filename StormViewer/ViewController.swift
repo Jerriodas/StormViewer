50de4fc23373265e429287b9c630bcd7a070f471
Jerriodas <zeus12gerardo@gmail.com>
2019-12-29 01:27:47 -0600

    implement tableView and read data

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +24,24 @@ class ViewController: UIViewController {
             for item in items {
                 if item.hasPrefix("nssl") {
                     //Load picture
+                    pictures.append(item)
                 }
             }
         }
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
 }
 
